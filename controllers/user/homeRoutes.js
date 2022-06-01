424de0f9c7aa923e52cbf71c2f054bb0ec0bdc56
cobalt88 <vincentteune@gmail.com>
2022-06-01 14:58:29 -0400

    building login routes

--- controllers/user/homeRoutes.js
+++ controllers/user/homeRoutes.js
@@ -33,19 +33,32 @@ router.get('/', (req, res, next) => {
 
   router.post('/login', async(req, res, next) => {
     try{
-      const dbUserData = await User.findOne({
+      const userData = await User.findOne({
         logging: console.log,
         where: {
           email: req.body.userEmail
         },
       });
-      if(!dbUserData){
+
+      if(!userData){
         res.status(400).json({ message: `The email address provided ${req.body.userEmail} is not registered. Try again with another email or sign up below.`})
+        return;
       }
+
+      req.session.save(() => {
+        req.session.loggedIn = true;
+  
+        res
+          .status(200)
+          .json({ user: dbUserData, message: 'You are now logged in!' });
+      });
+      
     }catch(err){
       console.error(`Unexpected error encountered in homeRoutes.js POST/login: ${err}`)
     }
   });
+
+  
 
   router.get('/register', (req, res, next) => {
     res.status(200).render('register', {
