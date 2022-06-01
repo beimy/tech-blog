cef01d4b8eee00068c713d94bacbc835b76db07a
cobalt88 <vincentteune@gmail.com>
2022-06-01 14:01:39 -0400

    6.1.22
    add routes map, update get all posts to async syntax

---
+++ controllers/route-map.md
@@ -0,0 +1,16 @@
+# Route Map
+
+## User Routes
+---
+
+* default route: http://localhost:3010/  ==> loads home page
+
+* /posts/  ==> GET all posts and posts data
+* /posts/{post_id} ==> GET posts by id
+* /posts/
+
+
+## Admin Routes
+---
+
+* not currently defined
--- controllers/user/postRoutes.js
+++ controllers/user/postRoutes.js
@@ -2,38 +2,91 @@ const router = require('express').Router();
 const { Post } = require('../../models');
 const { Comment } = require('../../models');
 const { User } = require('../../models');
+const { Tag } = require('../../models');
 
-router.get('/', (req, res) => {
-  Post.findAll({
-    attributes: [
-      'id',
-      'title',
-      'body',
-      'category',
-      'tags',
-      'user_id'
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'title', 'body', 'user_id', 'created_at'],
-        include: {
+router.get('/', async (req, res) => {
+
+  try{
+    const postData = await Post.findAll({
+      attributes: [
+        'id',
+        'title',
+        'body',
+        'category',
+        'tags',
+        'user_id'
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'title', 'body', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
           model: User,
           attributes: ['username']
+        },
+        {
+          model: Tag,
+          attributes: ['tag_name']
         }
-      },
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-  })
-    .then(dbPostData => res.json(dbPostData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+      ]
     });
+
+    res.status(200).json(postData);
+  }catch(err){
+    res.status(500).json(`Unexpected error encountered in GET all Posts route: ${err}`)
+  }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const postData = await Post.findByPk(postId, {
+      attributes: [
+        'id',
+        'title',
+        'body',
+        'category',
+        'tags',
+        'user_id'
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'title', 'body', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        },
+        {
+          model: Tag,
+          attributes: ['tag_name']
+        }
+      ]
+    });
+
+    res.status(200).json(postData)
+  } catch (err) {
+    res.status(500).json(`Unexpected error encountered in GET Post by id route: ${err}`)
+  }
+})
+
+// router.post('/new', withAuth async(req, res) => {
+//   try {
+//     const newPost = 
+//   } catch (err) {
+//     res.status(500).json(`Unexpected error encountered in POST New Post route: ${err}`)
+//   }
+// })
+  
 
 module.exports = router
