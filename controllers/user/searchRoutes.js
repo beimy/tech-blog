85c658360a8c96b846ffa9b8ed655a4b513d0ac8
Beimy <arbm159357@gmail.com>
2022-06-10 20:59:33 -0400

    tag id search is rendering a page, just not witht the info on it

--- controllers/user/searchRoutes.js
+++ controllers/user/searchRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Comment, Category, Tag } = require('../../models');
+const { User, Post, Comment, Category, Tag, Post_Tags } = require('../../models');
 const withAuth = require('../../utils/auth');
 const sequelize = require('../../config/connection');
 
@@ -69,6 +69,78 @@ router.get('/', async(req, res) => {
 
     }
     
-})
+});
+
+// GET POSTS BY TAG ID
+router.get('/tag/:id', async(req,res) => {
+    try {
+      const response = await Post_Tags.findAll({
+        where: {
+          tag_id: req.params.id
+        },
+        attributes: [
+          'tag_id',
+          'post_id'
+        ], 
+        include: [
+          {
+            model: Tag,
+            key: 'tag_id',
+            attributes: ['tag_name', 'tag_description']
+          },
+          {
+            model: Post,
+            key: 'post_id',
+            attributes: ['post_id', 'post_title','post_summary', 'post_content', 'category_id', 'post_url', 'user_id', 'created_at', 'updated_at' ],
+            include: [
+              {
+                model: User,
+                attributes: ['username']
+              },
+              {
+                model: Category,
+                attributes: ['category_name']
+              },
+              {
+                model: Comment,
+                attributes: ['comment_id', 'comment_title', 'comment_content', 'user_id'],
+                include: [
+                  {
+                    model: User,
+                    attributes: ['username']
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      })
+
+      const tagData = await Tag.findAll({
+        attributes: [
+          'tag_id',
+          'tag_name',
+          'tag_description'
+        ],
+      })
+    
+      const posts = response.map(post => post.get({ plain: true }));
+      const tags = tagData.map(tag => tag.get({ plain: true }));
+      console.log(posts)
+
+      res.status(200).render('index', {
+          tags,
+          posts,
+          loggedIn: req.session.loggedIn,
+          pageTitle: "search",
+          userNav: true,
+          mainCSS: true,
+          mainJS: true
+      });
+    } catch (err) {
+      res.status(500).json({message: `Unexpected error encountered in (route name here): ${err}`});
+      console.log(err);
+    }
+  });
 
 module.exports = router
