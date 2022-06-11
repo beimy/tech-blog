348a9f2822b5262c5dbc147282ee37f0dadad05a
Beimy <arbm159357@gmail.com>
2022-06-10 22:45:41 -0400

    please just connect

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,14 @@
-let sequelize;
+const Sequelize = require('sequelize');
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  });
-}
+require('dotenv').config();
+
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
 
 module.exports = sequelize;
