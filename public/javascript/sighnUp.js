86a0bfd4a7eace775d4495f4cd37294f660512c6
cobalt88 <vincentteune@gmail.com>
2022-06-11 17:00:32 -0400

    6.11.22
    update signup form

---
+++ public/javascript/sighnUp.js
@@ -0,0 +1,7 @@
+const registerBtn = document.getElementById('#registerBtn');
+const email = document.getElementById('#email');
+const username = document.getElementById('#username');
+const pass1 = document.getElementById('#password1');
+const pass2 = document.getElementById('#password2');
+
+console.log(email);
